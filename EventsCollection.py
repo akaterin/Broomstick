d81bdeec53ae019a082c1bd7672d19330d5e8df2
Katarzyna Kurzynska <akaterin@poczta.fm>
2014-11-04 20:06:02 +0100

    EventsCollection and SegmentCollection

--- EventsCollection.py
+++ EventsCollection.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from Queue import PriorityQueue
 
 class EventType(Enum):
     beggining = 1
@@ -11,8 +12,22 @@ class Event(object):
         self.type = type
         self.axis = axis
 
+    def __cmp__(self, other):
+        if self.axis < other.axis:
+            return 1
+        elif self.axis == other.axis:
+            return 0
+        else:
+            return -1
+
 
 class EventsCollection(object):
 
     def __init__(self):
-        self.events = []
+        self.events = PriorityQueue()
+
+    def add_event(self, event):
+        self.events.put(event)
+
+    def pop(self):
+        self.events.get()
--- SegmentCollection.py
+++ SegmentCollection.py
@@ -12,40 +12,46 @@ class SegmentCollection(object):
 
     def __init__(self):
         self.segments = []
-        #self.segments_struct = FastRBTree()
+        self.segments_struct = FastRBTree()
 
     def __init__(self, list):
-        self.segments = list
+        self.segments = []
+        self.segments_struct = FastRBTree()
+        map(lambda x: self.add_segment(x), list)
 
     def add_segment(self, segment):
         self.segments.append(segment)
-
-    def add_segment(self, start, end):
-        segment = Segment(start, end)
-        self.add_segment(segment)
+        self.segments_struct.insert(len(self.segments)-1, segment)
 
     def del_segment(self, segment):
+        key = self.segments.index(segment)
         self.segments.remove(segment)
+        self.segments_struct.remove(key)
 
     def del_segment(self, start, end):
         segment = Segment(start, end)
         self.del_segment(segment)
 
     def get(self, index):
-        return self.segments[index]
+        return self.segments_struct.get_value(index)
 
     def index(self, segment):
         return self.segments.index(segment)
 
     def move_up(self, segment):
         index = self.index(segment)
-        self.segments[index] = self.get(index -1)
+        parent = self.segments_struct.get_value(index-1)
+        self.segments_struct.remove(index-1)
+        self.segments_struct.remove(index)
+        self.segments_struct.insert(index-1, segment)
+        self.segments_struct.insert(index, parent)
+        self.segments[index] = parent
         self.segments[index -1] = segment
 
     def move_down(self, segment):
         index = self.index(segment)
-        self.segments[index] = self.get(index +1)
-        self.segments[index + 1] = segment
+        child = self.segments_struct.get_value(index+1)
+        self.move_up(child)
 
 
 
